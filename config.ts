5d804c1437154b47d108c9d1728e269e8e5538b3
Ziri Belabbas <ziri.belabbas@gmail.com>
2021-07-06 19:34:58 +0200

    fix config again

--- config.ts
+++ config.ts
@@ -1,9 +1,16 @@
-import { readFileSync } from "fs";
-const jsonConfig = JSON.parse(readFileSync('./config.json', 'utf-8'));
+import { readFileSync } from 'fs';
+let discordToken;
+let maxSoundDuration;
+
+try {
+    const jsonConfig = JSON.parse(readFileSync('./config.json', 'utf-8'));
+    discordToken = jsonConfig?.discordToken;
+    maxSoundDuration = jsonConfig?.maxSoundDuration;
+} catch {}
 
 const config = {
-    discordToken: jsonConfig?.discordToken ?? process.env.DISCORD_TOKEN,
-    maxSoundDuration: jsonConfig?.maxSoundDuration ?? parseInt(process.env.MAX_SOUND_DURATION!, 10)
+    discordToken: discordToken ?? process.env.DISCORD_TOKEN,
+    maxSoundDuration: maxSoundDuration ?? parseInt(process.env.MAX_SOUND_DURATION!, 10)
 };
 
 export default config;
